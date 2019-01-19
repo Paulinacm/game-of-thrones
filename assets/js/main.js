e9d2b7f8af5bb626adc9cf5171a28be582529451
Paulinacm <pauly.cm@gmail.com>
2019-01-19 14:57:48 -0300

    se subio archivo episodes en data.js, añadiendo estilo en css, index basico en html, cards funcionando en main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,24 @@
  * Acá va solo el manejo del DOM
  * 
  */
+const data = DATA.characters;
+//console.log(data)
+
+let arrCharacter = [];
+
+ //Card para mostrar todas la data
+ const showData = (data) => {
+     let result = '';
+     //console.log(data)
+     for (let i = 0; i < data.length; i++) {
+         document.getElementById("allgot").innerHTML += `
+          <div class = "card" style = "width: 18rem;"> 
+          <img class = "card-img-top" src = ${data[i].characterImageFull} alt = "Card image cap" >
+          <div class = "card-body">
+              <h5 class = "card-title"> ${data[i].characterName} </h5> 
+              <p class = "card-text"> House: ${data[i].houseName} </p>
+              </div>`         
+     }
+     return result;
+ };
+ window.onload = showData(data);
