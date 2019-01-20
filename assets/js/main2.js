7e6d7fbb09e699f423e624281dd5cc1ead2fd105
Paulinacm <pauly.cm@gmail.com>
2019-01-20 07:48:31 -0300

    filtro por temporada en main.js (no filtra)

--- assets/js/main2.js
+++ assets/js/main2.js
@@ -5,29 +5,46 @@
  */
 
 const dataep = EPISODES.episodes;
-const containerRoot = document.getElementById('root2');
-//console.log(dataep)
+const containerRoot = document.getElementById("root2");
+const selectSeason = document.getElementById("filterSeason1");
+//console.log(selectSeason)
+
+
  //SECCION VER INFORMACION CAPITULOS
  let arrEpisodes = [];
 
  //CARD PARA VER CAPITULOS
  const showDataep = (dataep) => {
      let result = '';
-     //console.log(data)
-    dataep.forEach(element => {
-        result = containerRoot.innerHTML += `
+     //console.log(dataep)
+    dataep.forEach(element => 
+            result = containerRoot.innerHTML += `
        <div class = "card" style = "width: 18rem;" >
            <div class = "card-body">
            <h5 class = "card-title"> ${element.episodeTitle} </h5>
            <p class = "card-text"> Temporada: ${element.seasonNum} </p>
            <p class = "card-text"> Description: ${element.episodeDescription} </p> </div>
        `
-    });
-    return result;
-    }
-    window.onload = showDataep(dataep);
+          )
+          return result;
+          }
 
+selectSeason.addEventListener('change', () => {
+    let condition = selectSeason.options[selectSeason.selectedIndex].text;
+    let filtered = filterSeason(dataep, condition);
+    console.log(filtered)
+    //console.log(condition)
+    // limpio div
+    containerRoot.innerHTML = '';
 
-
-      
-
+    filtered.forEach(element => {
+        containerRoot.innerHTML += `
+   <div class = "card" style = "width: 18rem;" >
+       <div class = "card-body">
+       <h5 class = "card-title"> ${element.episodeTitle} </h5> 
+       <p class = "card-text"> Temporada: ${element.seasonNum} </p> 
+       <p class = "card-text"> Descripción: ${element.episodeDescription} </p> </div> `
+    
+        })
+        })
+    window.onload = showDataep(dataep)
